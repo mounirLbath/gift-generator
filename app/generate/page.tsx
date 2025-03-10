2108998a5842d5ba753b14e7fead5f5a62b5c02c
Mounir LBATH <100532921+devmlGit@users.noreply.github.com>
2025-03-10 18:07:02 +0100

    Replace Link component with anchor tag for URL sharing and simplify href construction

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -22,12 +22,12 @@ const Home = () => {
           }}
           className="my-6 p-4 w-2/3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
-        <Link
+        <a
           className="my-10 text-blue-500 hover:text-blue-950"
-          href={{ pathname: "/", query: { m: encodeToBase64(message) } }}
+          href={"/?m=" + encodeToBase64(message)}
         >
           Send the URL of this page to your friend
-        </Link>
+        </a>
       </div>
       <Footer isGift={false} />
     </div>
